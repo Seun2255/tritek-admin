bc66e84b6a46625b6a24f8d89d50c81fd9db3630
Primal <81525899+Seun2255@users.noreply.github.com>
2022-07-07 05:03:52 +0100

    firebase setup

---
+++ pages/api/API.js
@@ -0,0 +1,21 @@
+import app from "../../firebase/firebaseApp";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, signInWithEmailAndPassword } from "firebase/firebase-auth";
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+const signIn = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      return user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("Not signed in", errorMessage);
+    });
+};
