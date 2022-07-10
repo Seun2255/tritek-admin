a5af7ecff9ea6e11d5eff5cf0a551e447bc5a558
Primal <81525899+Seun2255@users.noreply.github.com>
2022-07-10 05:28:26 +0100

    forms setup

---
+++ pages/forms/statement-form.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import styles from "../../styles/forms/statement-form.module.css";
+import logo from "../assets/logo.png";
+import email from "../assets/icons/email.svg";
+import { useState } from "react";
+
+export default function StatementForm() {
+  return <div className={styles.container}></div>;
+}
