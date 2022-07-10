0a9cc91643c77fad0a358083670b1e4814d42123
Primal <81525899+Seun2255@users.noreply.github.com>
2022-07-10 05:30:54 +0100

    build fix

--- pages/forms/candidate-query-form.js
+++ pages/forms/candidate-query-form.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "../../styles/forms/candidate-query-form.module.css";
-import logo from "../assets/logo.png";
-import email from "../assets/icons/email.svg";
+import logo from "../../assets/logo.png";
+import email from "../../assets/icons/email.svg";
 import { useState } from "react";
 
 export default function CandidateQueryForm() {
