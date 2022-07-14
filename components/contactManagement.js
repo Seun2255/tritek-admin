25cb96194d5d9eb6684a54c5c75f4a51c329b4bd
Primal <81525899+Seun2255@users.noreply.github.com>
2022-07-14 18:53:32 +0100

    feature: contact management page

--- components/contactManagement.js
+++ components/contactManagement.js
@@ -1,13 +1,102 @@
 import Image from "next/image";
-import styles from "../styles/components/contactManagement.module.css";
+import styles from "../styles/components/queries.module.css";
 import logo from "../assets/logo.png";
 import email from "../assets/icons/email.svg";
 import { useState } from "react";
+import { useEffect } from "react";
 
-export default function ContactManagement() {
+export default function Queries(props) {
+  const { data } = props;
+  const [fillUPArray, setFillUpArray] = useState([]);
+  const fillUp = () => {
+    var array = [];
+    if (data.length < 12) {
+      for (var i = 0; i < 12 - data.length; i++) {
+        array.push({
+          "First Name": "",
+          "Last Name": "",
+          "Phone number": "",
+          Emails: "",
+          Location: "",
+          Status: "",
+          Comments: "",
+          "User name": "",
+          "Date of Joining": "",
+          Roles: "",
+        });
+      }
+    }
+    setFillUpArray(array);
+  };
+
+  useEffect(() => {
+    fillUp();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Contact Management</h1>
+      <div className={styles.header}>
+        Contact Management - Queries (New, In Progress, Resolved)
+      </div>
+      <section className={styles.table__container}>
+        <table className={styles.table}>
+          <thead style={{ backgroundColor: "#CCCCCC" }}>
+            <tr className="table__head">
+              <td>Name</td>
+              <td className={styles.table__cell}>Role</td>
+              <td className={styles.table__cell}>Email</td>
+              <td className={styles.table__cell}>Phone Number</td>
+              <td className={styles.table__cell}>Country</td>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => {
+              return (
+                <tr
+                  className="table__row"
+                  key={index}
+                  style={{ backgroundColor: index % 2 ? "white" : "#DDDDDD" }}
+                >
+                  <td>{`${row["First Name"]} ${row["Last Name"]}`}</td>
+                  <td className={styles.table__cell}>{row["Roles"]}</td>
+                  <td className={styles.table__cell}>{row["Emails"]}</td>
+                  <td className={styles.table__cell}>{row["Phone number"]}</td>
+                  <td className={styles.table__cell}>{row["Location"]}</td>
+                </tr>
+              );
+            })}
+            {fillUPArray.map((row, index) => {
+              return (
+                <tr
+                  className="table__row"
+                  key={index}
+                  style={{ backgroundColor: index % 2 ? "#DDDDDD" : "white" }}
+                >
+                  <td>{`${row["First Name"]} ${row["Last Name"]}`}</td>
+                  <td className={styles.table__cell}>{row["Roles"]}</td>
+                  <td className={styles.table__cell}>{row["Emails"]}</td>
+                  <td className={styles.table__cell}>{row["Phone number"]}</td>
+                  <td className={styles.table__cell}>{row["Location"]}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
+      <div className={styles.pagination}>
+        <button
+          className={styles.back}
+          style={{
+            backgroundColor: "#6FA8DC",
+            width: "4em",
+            borderLeft: "2px solid black",
+          }}
+        >
+          {"<<"}
+        </button>
+        <button className={styles.back}>{"<"}</button>
+      </div>
     </div>
   );
 }
