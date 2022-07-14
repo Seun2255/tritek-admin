10458e7940d5d1bd394b320c97295ae6b657f28d
Primal <81525899+Seun2255@users.noreply.github.com>
2022-07-14 18:53:45 +0100

    feature: Queries page

--- components/queries.js
+++ components/queries.js
@@ -3,11 +3,107 @@ import styles from "../styles/components/queries.module.css";
 import logo from "../assets/logo.png";
 import email from "../assets/icons/email.svg";
 import { useState } from "react";
+import { useEffect } from "react";
 
-export default function Queries() {
+export default function Queries(props) {
+  const { data } = props;
+  const [fillUPArray, setFillUpArray] = useState([]);
+  const fillUp = () => {
+    var array = [];
+    if (data.length < 12) {
+      for (var i = 0; i < 12 - data.length; i++) {
+        array.push({
+          "First Name": "",
+          "Last Name": "",
+          "Query Number": "",
+          "Query Source": "",
+          "Query Type": "",
+          Subject: "",
+          "Phone number": "",
+          Emails: "",
+          Location: "",
+          Status: "",
+          Comments: "",
+        });
+      }
+    }
+    setFillUpArray(array);
+  };
+
+  useEffect(() => {
+    fillUp();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Queries</h1>
+      <div className={styles.header}>Queries - New, In progress, Resolved</div>
+      <section className={styles.table__container}>
+        <table className={styles.table}>
+          <thead style={{ backgroundColor: "#CCCCCC" }}>
+            <tr className={styles.table__head}>
+              <td>Name</td>
+              <td className={styles.table__cell}>Ticket Number</td>
+              <td className={styles.table__cell}>Address</td>
+              <td className={styles.table__cell}>Email</td>
+              <td className={styles.table__cell}>Phone Number</td>
+              <td className={styles.table__cell}>Comments</td>
+              <td className={styles.table__cell}>Attachments</td>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => {
+              return (
+                <tr
+                  className={styles.table__row}
+                  key={index}
+                  style={{
+                    backgroundColor: index % 2 ? "white" : "#DDDDDD",
+                  }}
+                >
+                  <td>{`${row["First Name"]} ${row["Last Name"]}`}</td>
+                  <td className={styles.table__cell}>{row["Query Number"]}</td>
+                  <td className={styles.table__cell}>{row["Location"]}</td>
+                  <td className={styles.table__cell}>{row["Emails"]}</td>
+                  <td className={styles.table__cell}>{row["Phone number"]}</td>
+                  <td className={styles.table__cell}>comments</td>
+                  <td className={styles.table__cell}>Attachments</td>
+                </tr>
+              );
+            })}
+            {fillUPArray.map((row, index) => {
+              return (
+                <tr
+                  className="table__row"
+                  key={index}
+                  style={{ backgroundColor: index % 2 ? "#DDDDDD" : "white" }}
+                >
+                  <td>{`${row["First Name"]} ${row["Last Name"]}`}</td>
+                  <td className={styles.table__cell}>{row["Query Number"]}</td>
+                  <td className={styles.table__cell}>{row["Location"]}</td>
+                  <td className={styles.table__cell}>{row["Emails"]}</td>
+                  <td className={styles.table__cell}>{row["Phone number"]}</td>
+                  <td className={styles.table__cell}></td>
+                  <td className={styles.table__cell}></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
+      <div className={styles.pagination}>
+        <button
+          className={styles.back}
+          style={{
+            backgroundColor: "#6FA8DC",
+            width: "4em",
+            borderLeft: "2px solid black",
+          }}
+        >
+          {"<<"}
+        </button>
+        <button className={styles.back}>{"<"}</button>
+      </div>
     </div>
   );
 }
