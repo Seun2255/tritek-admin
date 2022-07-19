35e4f3b2a8f932facaccf5c65945c55da09d8780
Primal <81525899+Seun2255@users.noreply.github.com>
2022-07-19 13:14:07 +0100

    feature: escalatte query

---
+++ components/escalateQuery.js
@@ -0,0 +1,90 @@
+import Image from "next/image";
+import styles from "../styles/components/escalateQuery.module.css";
+import { useState } from "react";
+import arrow from "../assets/icons/arrow-black.svg";
+import axios from "axios";
+
+export default function EscalateQuery(props) {
+  const { data, setEscalateQuery, setViewQuery, staff } = props;
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [selectedStaff, setSelectedStaff] = useState({ Emails: "Staff" });
+
+  const handleSubmit = () => {
+    axios
+      .post("https://tritek-mail.herokuapp.com/api/escalate", {
+        email: selectedStaff.Emails,
+        ticket: data["Query Number"],
+        query: data.Comments,
+      })
+      .then(function (response) {
+        console.log(response);
+        console.log("Succes");
+        setEscalateQueryQuery(false);
+        setViewQuery(true);
+      })
+      .catch(function (error) {
+        console.log("Failed");
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className={styles.outer}>
+      <div
+        className={styles.back__button}
+        onClick={() => {
+          setEscalateQuery(false);
+          setViewQuery(true);
+        }}
+      >
+        Back
+      </div>
+      <div className={styles.container}>
+        <div className={styles.header}>Escalate</div>
+        <div className={styles.staff__box}>
+          <div className={styles.home__buttons}>
+            <button
+              className={styles.menu__button}
+              onClick={() => setMenuOpen(!menuOpen)}
+            >
+              <span>{selectedStaff.Emails}</span>
+              <div className={styles.dropdown__arrow}>
+                <Image
+                  alt="arrow"
+                  layout="fill"
+                  src={arrow}
+                  style={{
+                    transform: menuOpen ? "rotate(180deg)" : "rotate(0deg)",
+                  }}
+                />
+              </div>
+            </button>
+            {menuOpen && (
+              <div className={styles.menu}>
+                {staff.map((option, id) => {
+                  return (
+                    <div
+                      key={id}
+                      className={styles.staff}
+                      onClick={() => {
+                        setSelectedStaff(option);
+                      }}
+                    >
+                      {option.Emails}
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className={styles.base}></div>
+      </div>
+      <div className={styles.action__buttons}>
+        <button className={styles.action__button} onClick={handleSubmit}>
+          Escalate
+        </button>
+      </div>
+    </div>
+  );
+}
